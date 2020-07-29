359824679ad079eb94dbc3b25b11835e76b73808
Marko-tesla <prokudinmarat@gmail.com>
2020-07-29 17:42:44 +0600

    initial commit

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
+
+    echo $controllerName . '<br/>';
+
+    $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+    echo 'Func form str'.$actionName . '<br/>';
+
+    include_once '../controllers/'.$controllerName.'Controller.php';
